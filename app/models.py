ef5bc5c7d776b29892cbe81da424bd0181b9cb19
medeeab8 <medeea.teodora8@gmail.com>
2024-06-01 11:18:35 +0300

    Added user class

--- app/models.py
+++ app/models.py
@@ -6,3 +6,10 @@ class Meal(db.Model):
     description = db.Column(db.String(200), nullable=True)
     calories = db.Column(db.Integer, nullable=False)
     date = db.Column(db.String(20), nullable=False)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), nullable=False)
+    height = db.Column(db.Integer, nullable=False)  # height in cm
+    weight = db.Column(db.Integer, nullable=False)  # weight in kg
+    activity_level = db.Column(db.Integer, nullable=False)  # activity level from 1 to 5
