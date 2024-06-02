ad34fbe3909e0b3d2d836a4fa9659d39e3057789
medeeab8 <medeea.teodora8@gmail.com>
2024-06-02 17:41:13 +0300

    added comments to the code

--- app/routes.py
+++ app/routes.py
@@ -13,30 +13,36 @@ bp = Blueprint('routes', __name__)
 def index():
   return "Hello, world!"
 
+# Route for adding a meal
 @bp.route('/add_meal', methods=['POST'])
 def add_meal():
-    
+    # Extract meal details from request JSON
     name = request.json['name']
     username = request.json['username']
     description = request.json.get('description', '')
     calories = request.json['calories']
     date = request.json.get('date', datetime.now().strftime('%Y-%m-%d'))
-
+    
+    # Check if the user exists
     existing_user = User.query.filter_by(username=username).first()
     if not existing_user:
         return jsonify({"error": f"Username '{username}' does not exist. Please register first."}), 400
-
+    
+    # Create a new meal
     new_meal = Meal(name=name, username=username, description=description, calories=calories, date=date)
     db.session.add(new_meal)
     db.session.commit()
     
+    # Calculate total calories consumed by the user for the day
     total_calories_consumed = db.session.query(db.func.sum(Meal.calories)).filter_by(username=username, date=date).scalar()
     
     if total_calories_consumed is None:
         total_calories_consumed = 0
-
+    
+    # Calculate remaining calories based on TDEE
     remaining_calories = existing_user.tdee - total_calories_consumed
-
+    
+    # Determine message based on remaining calories
     if remaining_calories > 0:
         message = "Meal added successfully."
     else:
@@ -49,28 +55,36 @@ def add_meal():
         "remaining_calories": remaining_calories
     })
 
+# Route for retrieving meals by username
 @bp.route('/meals/<username>', methods=['GET'])
 def get_meals(username): 
+    # Fetch meals based on username if provided, otherwise fetch all meals
     if username:
         meals = Meal.query.filter_by(username=username).all()
     else:
         meals = Meal.query.all()
     return meals_schema.jsonify(meals)
 
+# Route for retrieving a single meal by ID
 @bp.route('/get_meal/<int:id>', methods=['GET'])
 def get_meal(id):
+    # Retrieve meal by ID or return 404 if not found
     meal = Meal.query.get_or_404(id)
     return meal_schema.jsonify(meal)
 
+# Route for updating a meal
 @bp.route('/update_meal/<int:id>', methods=['PUT'])
 def update_meal(id):
+    # Retrieve meal by ID or return 404 if not found
     meal = Meal.query.get_or_404(id)
 
     json_data = request.json
-
+    
+    # Check if the user is authorized to update this meal
     if 'username' not in json_data or json_data['username'] != meal.username:
         return jsonify({"error": "You are not authorized to update this meal."}), 403
     
+    # Update meal details if provided in the request
     if 'name' in json_data:
         meal.name = json_data['name']
     if 'username' in json_data:
@@ -82,29 +96,37 @@ def update_meal(id):
     if 'date' in json_data:
         meal.date = json_data['date']
     
+    # Check if the user exists
     existing_user = User.query.filter_by(username=meal.username).first()
     if not existing_user:
         return jsonify({"error": f"Username '{meal.username}' does not exist. Please register first."}), 400
-
-    db.session.commit()
     
+    # Commit changes to the database
+    db.session.commit()
+    # Return the updated meal
     return meal_schema.jsonify(meal)
 
+# Route for deleting a meal by ID
 @bp.route('/meals/<int:id>', methods=['DELETE'])
 def delete_meal(id):
+    # Retrieve meal by ID or return 404 if not found
     meal = Meal.query.get_or_404(id)
+    # Delete the meal from the database
     db.session.delete(meal)
     db.session.commit()
-    
+    # Return empty response with 204 status code
     return '', 204
 
+# Route for adding a user
 @bp.route('/add_user', methods=['POST'])
 def add_user():
+    # Extract user details from request JSON
     username = request.json['username']
     height = request.json['height']
     weight = request.json['weight']
     activity_level = request.json['activity_level']
-
+    
+    # Check if the username already exists
     existing_user = User.query.filter_by(username=username).first()
     if existing_user:
         return jsonify({"error": f"Username '{username}' already exists"}), 400
@@ -115,18 +137,24 @@ def add_user():
     # Create a new user
     new_user = User(username=username, height=height, weight=weight, activity_level=activity_level, tdee=tdee)
 
+    # Commit changes to the database
     db.session.add(new_user)
     db.session.commit()
 
+    # Returm user_schema
     return user_schema.jsonify(new_user)
 
+# Route for updating a user by ID
 @bp.route('/update_user/<int:id>', methods=['PUT'])
 def update_user(id):
+    # Retrieve the user by ID or return 404 if not found
     user = User.query.get_or_404(id)
 
+    # Get JSON data from the request
     json_data = request.json
     tdee_reassession = False
 
+    # Update user details if provided in the request
     if 'username' in json_data:
         user.username = json_data['username']
     if 'height' in json_data and user.height != json_data['height']:
@@ -139,6 +167,7 @@ def update_user(id):
         user.activity_level = json_data['activity_level']
         tdee_reassession = True
 
+    # Reassess TDEE if any user details affecting TDEE are updated
     if tdee_reassession:
         tdee = calculate_tdee(user.height, user.weight, user.activity_level)
         user.tdee = tdee
@@ -148,18 +177,23 @@ def update_user(id):
 
     db.session.commit()
 
+    # Return JSON response with the update message and user details
     return jsonify({
         "message":message,
         "user":user_schema.dump(user)})
 
+# Route for deleting a user by ID
 @bp.route('/delete_user/<int:id>', methods=['DELETE'])
 def delete_user(id):
+    # Retrieve the user by ID or return 404 if not found
     user = User.query.get_or_404(id)
+    # Delete the user from the database
     db.session.delete(user)
     db.session.commit()
-    
+    # Return empty response with 204 status code
     return '', 204
 
+# Error handler for 404 Not Found errors
 @bp.errorhandler(404)
 def not_found(error):
     return jsonify({'message': 'Not found'}), 404
--- tests/test_meals.py
+++ tests/test_meals.py
@@ -5,45 +5,56 @@ import json
 
 @pytest.fixture
 def client():
+    # Create a Flask app instance for testing
     app = create_app()
     app.config['TESTING'] = True
     client = app.test_client()
 
+    # Create all database tables
     with app.app_context():
         db.create_all()
         yield client
+        # Drop all database tables after testing 
         db.drop_all()
 
 def test_get_meals_by_username(client):
+    # Create a user and add to the database
     user = User(username='john_doe', height=175, weight=70, activity_level=2)
     db.session.add(user)
     db.session.commit()
-
+    
+    # Create meals for the user
     meal1 = Meal(name='Grilled Chicken Salad', username='john_doe', description='Grilled chicken breast with mixed greens and vinaigrette', calories=350, date='2024-06-02')
     meal2 = Meal(name='Pasta Carbonara', username='john_doe', description='Spaghetti with creamy sauce, bacon, and parmesan cheese', calories=600, date='2024-06-02')
     db.session.add(meal1)
     db.session.add(meal2)
     db.session.commit()
 
+    # Test retrieving meals for the user
     response = client.get('/meals/john_doe')
     assert response.status_code == 200
 
+    # Verify the retrieved meals
     meals = response.get_json()
     assert len(meals) == 2
     assert meals[0]['name'] == 'Grilled Chicken Salad'
     assert meals[1]['name'] == 'Pasta Carbonara'
 
+    # Test retrieving meals for a nonexistent user
     response = client.get('/meals/nonexistent_user')
     assert response.status_code == 200
 
+    # Verify no meals are retrieved for the nonexistent user
     meals = response.get_json()
     assert len(meals) == 0
 
 def test_add_meal(client):
+    # Create a test user and add to the database
     test_user = User(username='testuser', height=180, weight=75, activity_level=3, tdee=2500)
     db.session.add(test_user)
     db.session.commit()
 
+    # Data for the meal to be added
     meal_data = {
         'name': 'Chicken Salad',
         'username': 'testuser',
@@ -51,9 +62,10 @@ def test_add_meal(client):
         'calories': 350,
         'date': '2024-05-31'
     }
-
+    # Test adding the meal
     response = client.post('/add_meal', data=json.dumps(meal_data), content_type='application/json')
 
+    # Verify the response
     assert response.status_code == 200
     response_data = json.loads(response.data)
 
@@ -66,6 +78,7 @@ def test_add_meal(client):
     assert 'remaining_calories' in response_data
 
 def test_add_meal_user_not_exist(client):
+    # Data for a meal with a non-existent user
     meal_data = {
         'name': 'Chicken Salad',
         'username': 'nonexistentuser',
@@ -73,9 +86,10 @@ def test_add_meal_user_not_exist(client):
         'calories': 350,
         'date': '2024-05-31'
     }
-
+    # Test adding the meal
     response = client.post('/add_meal', data=json.dumps(meal_data), content_type='application/json')
-
+    
+    # Verify the response
     assert response.status_code == 400
     response_data = json.loads(response.data)
 
@@ -83,6 +97,7 @@ def test_add_meal_user_not_exist(client):
 
 
 def test_get_meal(client):
+    # Create a user and a meal for testing
     test_user = User(username='testuser', height=180, weight=75, activity_level=3, tdee=2500)
     db.session.add(test_user)
     db.session.commit()
@@ -91,8 +106,10 @@ def test_get_meal(client):
     db.session.add(test_meal)
     db.session.commit()
 
+    # Test retrieving the meal 
     response = client.get(f'/get_meal/{test_meal.id}')
 
+    # Verify respose
     assert response.status_code == 200
     response_data = json.loads(response.data)
     assert response_data['name'] == 'Dinner'
@@ -101,6 +118,7 @@ def test_get_meal(client):
     assert response_data['date'] == '2024-05-30'
 
 def test_update_meal(client):
+    # Create a user and a meal for testing
     user = User(username='john_doe', height=175, weight=70, activity_level=2)
     db.session.add(user)
     db.session.commit()
@@ -109,6 +127,7 @@ def test_update_meal(client):
     db.session.add(meal)
     db.session.commit()
 
+    # Test updating the meal
     response = client.put(f'/update_meal/{meal.id}', json={
         'username': 'john_doe',
         'name': 'Updated Chicken Salad',
@@ -118,12 +137,14 @@ def test_update_meal(client):
     })
     assert response.status_code == 200
 
+    # Verify the response
     updated_meal = response.get_json()
     assert updated_meal['name'] == 'Updated Chicken Salad'
     assert updated_meal['description'] == 'Updated description'
     assert updated_meal['calories'] == 400
     assert updated_meal['date'] == '2024-06-03'
 
+    # Test updating the meal with unauthorized user
     response = client.put(f'/update_meal/{meal.id}', json={
         'username': 'jane_doe',
         'name': 'Unauthorized Update'
@@ -132,6 +153,7 @@ def test_update_meal(client):
     error_message = response.get_json()
     assert error_message['error'] == 'You are not authorized to update this meal.'
 
+    # Test updating the meal with a non-existent user
     response = client.put(f'/update_meal/{meal.id}', json={
         'username': 'non_existent_user',
         'name': 'Update with non-existent user'
@@ -141,6 +163,7 @@ def test_update_meal(client):
     assert error_message['error'] == 'You are not authorized to update this meal.'
 
 def test_delete_meal(client):
+    # Create a test user and a meal for testing
     test_user = User(username='testuser', height=180, weight=75, activity_level=3, tdee=2500)
     db.session.add(test_user)
     db.session.commit()
@@ -149,12 +172,15 @@ def test_delete_meal(client):
     db.session.add(test_meal)
     db.session.commit()
 
+    # Test deleting the meal
     response = client.delete(f'/meals/{test_meal.id}')
 
+    # Verify the response
     assert response.status_code == 204
     assert Meal.query.get(test_meal.id) is None
 
 def test_add_user(client):
+    # Data for adding a new user
     user_data = {
         'username': 'newuser',
         'height': 170,
@@ -162,8 +188,10 @@ def test_add_user(client):
         'activity_level': 3
     }
 
+    # Test adding the user
     response = client.post('/add_user', data=json.dumps(user_data), content_type='application/json')
-
+    
+    # Verify the response
     assert response.status_code == 200
     response_data = json.loads(response.data)
     assert response_data['username'] == 'newuser'
@@ -173,19 +201,23 @@ def test_add_user(client):
     assert 'tdee' in response_data
 
 def test_add_user_existing_username(client):
+    # Create a test user with an existing username
     test_user = User(username='testuser', height=180, weight=75, activity_level=3, tdee=2500)
     db.session.add(test_user)
     db.session.commit()
-
+    
+    # Data for adding a new user with the same username
     user_data = {
         'username': 'testuser',
         'height': 170,
         'weight': 70,
         'activity_level': 3
     }
-
+  
+    # Test adding the user
     response = client.post('/add_user', data=json.dumps(user_data), content_type='application/json')
-
+    
+    # Verify the response
     assert response.status_code == 400
     response_data = json.loads(response.data)
     assert response_data['error'] == "Username 'testuser' already exists"
@@ -193,19 +225,23 @@ def test_add_user_existing_username(client):
 
 
 def test_update_user(client):
+    # Create a test user
     test_user = User(username='testuser', height=180, weight=75, activity_level=3, tdee=2500)
     db.session.add(test_user)
     db.session.commit()
-
+    
+    # Data for updating the user
     update_data = {
         'username': 'updateduser',
         'height': 175,
         'weight': 72,
         'activity_level': 4
     }
-
+    
+    # Test updating the user
     response = client.put(f'/update_user/{test_user.id}', data=json.dumps(update_data), content_type='application/json')
-
+    
+    # Verify the response
     assert response.status_code == 200
     response_data = json.loads(response.data)
     assert response_data['user']['username'] == 'updateduser'
@@ -216,27 +252,34 @@ def test_update_user(client):
     assert 'tdee' in response_data['user']
 
 def test_update_user_no_tdee_reassessment(client):
+    # Create a test user
     test_user = User(username='testuser', height=180, weight=75, activity_level=3, tdee=2500)
     db.session.add(test_user)
     db.session.commit()
 
+    # Data for updating the user without reassessing TDEE
     update_data = {
         'username': 'updateduser'
     }
 
+    # Test updating the user
     response = client.put(f'/update_user/{test_user.id}', data=json.dumps(update_data), content_type='application/json')
-
+    
+    # Verify the response
     assert response.status_code == 200
     response_data = json.loads(response.data)
     assert response_data['user']['username'] == 'updateduser'
     assert response_data['message'] == "User updated successfully!"
 
 def test_delete_user(client):
+    # Create a test user
     test_user = User(username='testuser', height=180, weight=75, activity_level=3, tdee=2500)
     db.session.add(test_user)
     db.session.commit()
 
+    # Test deleting the user
     response = client.delete(f'/delete_user/{test_user.id}')
 
+    # Verify the response
     assert response.status_code == 204
     assert User.query.get(test_user.id) is None
